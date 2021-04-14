27c103d54fda73ce1ea4dd6aabefb5cbe5a1b867
swimmerbee <bryce@thewalthers.com>
2021-04-14 12:22:42 -0700

    Fixed saveFile and echoing of command characters

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include <ncurses.h>
 #include <fstream>
+#include <vector>
 
-void initCurses() {
-  initscr();
-  noecho();
-  cbreak();
-  keypad(stdscr, true);
-}
 
-std::string readFile() {
+std::string readFile(std::vector<char> &data) {
   erase();
   //get fn
   char fn[80];
   getstr(fn);
   erase();
 
-  // reading in the file
   std::ifstream read(fn);
 
   if(read.is_open()) {
@@ -23,9 +17,19 @@ std::string readFile() {
       // make var, read in curr line, add to scr, refresh
       std::string line = "";
       getline(read, line);
+
+      // Convert line to c style string
       const char * ln = line.c_str();
+
+      // Add the chars to the save vector
+      int size = line.size();
+      for (int i = 0; i < size; i++) {
+          data.push_back(ln[i]);
+      }
+
       addstr(ln);
       addstr("\n");
+      data.push_back('\n');
       refresh();
     }
 
@@ -35,23 +39,23 @@ std::string readFile() {
   return fileName;
 }
 
-int saveFile(std::string fn) {
+void saveFile(std::string fn, std::vector<char> saveData) {
   // if filename is empty, make it untitled
   if (fn == "") {
-    fn = "untitled";
+    fn = "untitled.txt";
   }
 
   // Convert string to const char
   const char * fileName = fn.c_str();
 
-  // Put contentents of screen to file
-  int success = 0;
-  success = scr_dump(fileName);
-
-  if (success == 0) {
-    addstr("An error occured while saving.\n");
+  std::ofstream save(fn);
+  if(save.is_open()) {
+    for (int i = 0; i < saveData.size(); i++) {
+      save << saveData[i];
+    }
   }
-  return success;
+  save.close();
+
 }
 
 int main() {
@@ -60,46 +64,60 @@ int main() {
   char c;
   int x = 0;
   int y = 0;
-  int test = 0;
-  int unimp = 0;
+  std::vector<char> data;
+
   initscr();
+  keypad(stdscr, true);
+  noecho();
+  raw();
 
   while(run == true) {
     c = getch();
+    data.push_back(c);
     getyx(stdscr, y, x);
 
     switch (c) {
-      case KEY_BACKSPACE:
-        unimp = mvdelch(y, x-1);
+      case 263:
+        mvdelch(y, x-1);
         break;
-      case KEY_DOWN:
-        unimp = move(y-1, x);
+      case 258:
+        y++;
+        move(y, x);
         break;
-      case KEY_UP:
-        unimp = move(y+1, x);
+      case 259:
+        if(y-1 >= 0) {
+          y--;
+          move(y, x);
+        }
         break;
-      case KEY_RIGHT:
-        unimp = move(y, x+1);
+      case 261:
+        x++;
+        move(y, x);
         break;
-      case KEY_LEFT:
-        unimp = move(y, x-1);
+      case 260:
+        if(x-1 >= 0) {
+          x--;
+          move(y, x);
+        }
         break;
       case 15:
-        currFileName = readFile();
+        data.erase(data.begin(), data.end());
+        currFileName = readFile(data);
         break;
-      case 19:
-        test = saveFile("");
-        addch(test);
+      case 1:
+        data.pop_back();
+        saveFile(currFileName, data);
         break;
       case 24:
-        endwin();
         run = false;
         break;
       default:
+        addch(c);
         refresh();
         break;
     }
   }
 
+  endwin();
   return 0;
 }
